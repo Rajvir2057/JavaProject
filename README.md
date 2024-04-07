fb5a7fee6c5c597e0e718cd87a2dc72f88f30444
Rajvir2057 <rajvirkaur2057@gmail.com>
2024-04-07 21:05:12 +0300

    one task

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JavaProject
+### time greet
+## it is just a simple design that uses the users time input to greet them.
---
+++ TimeGreetingGame/Timegreeting.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Timegreeting{
+    public static void main(String[] args){
+
+        //To show what output format is needed.
+        System.out.println("please enter the time in 24 hr timing..");
+
+        // creating a scanner to take the hour and min from the user.
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter your name please: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter the hour: ");
+        int hour = input.nextInt();
+
+        System.out.print("Enter the minute: ");
+        int minute = input.nextInt();
+        
+        // if for getting the input to greet..
+        
+        if((hour <= 1) && (minute >= 0)){
+            System.out.println(name + ", Good Morning!");
+        }
+
+        else if((hour <= 12) && (minute >= 0)){
+            System.out.println(name + ", Good Afternoon!");
+        }
+
+        else if((hour <= 18) && (minute >= 0)){
+            System.out.println(name + ", Good Evening!");
+        }
+        
+        else if((hour <= 21) && (minute >= 0)){
+            System.out.println(name + ", Good Night!");
+        }
+
+        else{
+        System.out.println("hmmm... wonder y.");
+        }
+        // if you dont know the time... 
+        input.close();
+    }
+}
