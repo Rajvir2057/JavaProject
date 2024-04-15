b6195f1272b97e041863f6e2f5aa8488a5a8c867
Rajvir2057 <rajvirkaur2057@gmail.com>
2024-04-15 12:44:04 +0300

    bank

---
+++ StationaryShop/RajStationary.java
@@ -0,0 +1,17 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import java.awt.Color;
+
+public class RajStationary {
+    
+            // setting up my main frame.
+            JFrame frame = new JFrame();
+
+            frame.setTitle("RajStationary");
+            frame.setSize(500, 400);
+            frame.setResizable(false);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+            
+            frame.getContentPane().setBackground(new Color(0xE8F8FC));
+}
